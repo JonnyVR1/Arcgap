d661e1564ae044ea2f6403dfaaf843e3d460b1e3
Jonathon Fitch <lightman1@virginmedia.com>
2022-03-28 02:03:45 +0100

    Initial Commit

---
+++ Arcgap/Plugin.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using IPA;
+using IPA.Logging;
+using JetBrains.Annotations;
+
+namespace Arcgap
+{
+    [UsedImplicitly]
+	[Plugin(RuntimeOptions.DynamicInit)]
+	public class Plugin
+	{
+		internal static Logger Log { get; private set; }
+
+		internal static Harmony Harmony { get; private set; }
+
+		[Init]
+		public Plugin(Logger logger)
+		{
+			Log = logger;
+			Harmony = new Harmony("Kinsi55.BeatSaber.Arcgap");
+		}
+
+        [UsedImplicitly]
+		[OnEnable]
+		public void OnEnable()
+		{
+            Harmony.PatchAll(Assembly.GetExecutingAssembly());
+		}
+
+        [UsedImplicitly]
+		[OnDisable]
+		public void OnDisable()
+		{
+			Harmony.UnpatchSelf();
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Beat Saber Arcgap
+Fixes game softlock / crash on 1.20+
