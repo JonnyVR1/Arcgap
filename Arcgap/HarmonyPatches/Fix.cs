fa608eb3397e9e452b20f9c9328951d312d50631
Jonathon Fitch <lightman1@virginmedia.com>
2022-03-28 02:03:45 +0100

    Initial Commit

---
+++ Arcgap/HarmonyPatches/Fix.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using HarmonyLib;
+using JetBrains.Annotations;
+
+namespace Arcgap.HarmonyPatches
+{
+    [HarmonyPatch(typeof (BeatmapCallbacksController), "ManualUpdate")]
+    internal static class Fix
+    {
+        [UsedImplicitly]
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var codeMatcher = new CodeMatcher(instructions);
+            codeMatcher
+                .End()
+                .MatchBack(true, new(OpCodes.Ble, null, "exit"),
+                    new(OpCodes.Ldloc_S),
+                    new(OpCodes.Ldfld, AccessTools.Field(typeof (BeatmapDataItem), "type"))).ThrowIfInvalid("j");
+
+            var operand = codeMatcher.NamedMatch("exit").operand;
+
+            codeMatcher
+                .MatchForward(false, new(OpCodes.Bne_Un),
+                    new(OpCodes.Ldloc_S),
+                    new(OpCodes.Castclass, typeof (BeatmapEventData))).ThrowIfInvalid("j2").Operand = operand;
+
+            return codeMatcher.InstructionEnumeration();
+        }
+    }
+}
---
+++ Arcgap/Plugin.cs
@@ -0,0 +1,46 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using IPA;
+using IPA.Logging;
+using IPA.Utilities;
+using JetBrains.Annotations;
+
+namespace Arcgap
+{
+    [UsedImplicitly]
+	[Plugin(RuntimeOptions.DynamicInit)]
+	public class Plugin
+	{
+		internal static Logger Log { get; private set; }
+
+		internal static Harmony Harmony { get; private set; }
+
+		[Init]
+		public Plugin(Logger logger)
+		{
+			Log = logger;
+			Harmony = new Harmony("Kinsi55.BeatSaber.Arcgap");
+		}
+
+        [UsedImplicitly]
+		[OnEnable]
+        public void OnEnable()
+        {
+            var gameVersion = UnityGame.GameVersion;
+            if (gameVersion < new AlmostVersion("1.20.0") || gameVersion >= new AlmostVersion("1.24.0"))
+            {
+                Log.Warn("Plugin not enabled - This bug should probably fixed in Game Versions after 1.20-1.23");
+                return;
+            }
+
+            Harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        [UsedImplicitly]
+		[OnDisable]
+		public void OnDisable()
+		{
+			Harmony.UnpatchSelf();
+		}
+	}
+}
